cdb2b8af8709047ee1384dc9dba40bb70ff2457a
Ishrafil <ishrafil.webdev@gmail.com>
2024-06-11 00:34:33 +0600

    update

--- src/features/api/admin/adminManufactureApi.js
+++ src/features/api/admin/adminManufactureApi.js
@@ -3,6 +3,8 @@ import { adminBaseApi } from "./adminBaseApi";
 const adminManufactureApi = adminBaseApi.injectEndpoints({
   tagTypes: ["Manufactures"],
   endpoints: (builder) => ({
+    
+    // get all manufacturer 
     getAllManufacture: builder.query({
       query: () => "/manufacture/all",
       transformResponse: (response) => {
@@ -11,7 +13,37 @@ const adminManufactureApi = adminBaseApi.injectEndpoints({
       },
       providesTags: ["Manufactures"],
     }),
+
+    // add manufacturer
+    addManufacturer: builder.mutation({
+      query: (manufacturerData) => ({
+        url: "/manufacturer/create",
+        method: "POST",
+        body: manufacturerData,
+      }),
+      invalidatesTags: ["Manufactures"],
+    }),
+
+    // update manufacturer
+    // updateManufacturer: builder.mutation({
+    //   query: ({ id, manufacturerData }) => ({
+    //     url: `/manufacturer/update/${id}`,
+    //     method: "PUT",
+    //     body: manufacturerData,
+    //   }),
+    //   invalidatesTags: ["Manufactures"],
+    // }),
+
+    // delete manufacturer
+    // deleteManufacturer: builder.mutation({
+    //   query: (id) => ({
+    //     url: `/manufacturer/delete/${id}`,
+    //     method: "DELETE",
+    //   }),
+    //   invalidatesTags: ["Manufactures"],
+    // }),
   }),
 });
 
-export const { useGetAllManufactureQuery } = adminManufactureApi;
+export const { useGetAllManufactureQuery, useUpdateManufacturerMutation } =
+  adminManufactureApi;
