8517587b2ad00d49e24e232fae561f6dcef8228a
Ishrafil <ishrafil.webdev@gmail.com>
2024-07-05 11:12:37 +0600

    update navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,18 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.png";
 import Cookies from "js-cookie";
+import { useDispatch } from "react-redux";
+import { getUser } from "../../features/auth/authSlice";
+// import CreateAdmin from './CreateAdmin'
 
 const Navbar = () => {
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
-  const user = Cookies.get("email") || null;
+  const [user, setUser] = useState("");
+  const [userData, setUserData] = useState(false);
+  const userEmail = Cookies.get("email") || null;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const res = await dispatch(getUser(userEmail)).unwrap();
+        const user = res?.data?.email;
+        setUser(user);
+      } catch (error) {
+        console.error("Failed to fetch user data:", error);
+      } finally {
+        setUserData(true);
+      }
+    };
+    fetchUserData();
+  }, [dispatch, userEmail]);
 
   const toggleMobileMenu = () => {
     setMobileMenuOpen(!isMobileMenuOpen);
   };
 
   return (
-    <nav className="bg-[#006E9E] text-white">
+    <nav className="bg-[#006E9E] text-white h-16">
       <div className="container mx-auto px-5 md:px-12 py-3 md:py-4 flex items-center justify-between">
         {/* common portion for mobile and large device  */}
         <Link to="/" className="flex items-center gap-x-2">
@@ -25,37 +46,45 @@ const Navbar = () => {
           <div className="flex items-center gap-x-4">
             <Link
               to="/subscription-plan"
-              className="text-white hover:text-gray-300 text-sm font-semibold"
+              className="text-white flex-grow hover:text-gray-300 text-sm font-semibold"
             >
               Subscriptions
             </Link>
-            {user ? (
-              <Link
-                to="/dashboard"
-                className="text-white hover:text-gray-300 text-sm font-semibold"
-              >
-                Dashboard
-              </Link>
-            ) : (
-              <div className="flex gap-x-4">
-                <Link
-                  to="/login"
-                  className="text-sm border border-white px-4 py-2 rounded hover:bg-white hover:text-[#006E9E]"
-                >
-                  Sign in
-                </Link>
-                <Link
-                  to="/register"
-                  className="text-sm bg-red-700 px-4 py-2 rounded text-white hover:bg-red-800"
-                >
-                  Register
-                </Link>
+            {
+              !userData && <div className="flex-grow w-20"></div>
+            }
+            <div className="h-full w-full">
+              {userData &&
+                (user ? (
+                  <Link
+                    to="/dashboard"
+                    className="text-white flex-grow hover:text-gray-300 text-sm font-semibold"
+                  >
+                    Dashboard
+                  </Link>
+                ) : (
+                  <div className="flex gap-x-4 flex-grow">
+                    <Link
+                      to="/login"
+                      className="text-sm border border-white px-4 py-2 rounded hover:bg-white hover:text-[#006E9E]"
+                    >
+                      Sign in
+                    </Link>
+                    <Link
+                      to="/register"
+                      className="text-sm bg-red-700 px-4 py-2 rounded text-white hover:bg-red-800"
+                    >
+                      Register
+                    </Link>
 
-                {/* for temporary create a admin  */}
-                {/* <CreateAdmin/> */}
-              </div>
-            )}
+                    {/* for temporary create a admin  */}
+                    {/* <CreateAdmin/> */}
+                  </div>
+                ))}
+            </div>
           </div>
+          {/* for temporary create a admin  */}
+          {/* <CreateAdmin/> */}
         </div>
 
         {/* mobile view menu button  */}
@@ -87,18 +116,30 @@ const Navbar = () => {
         >
           Subscriptions
         </Link>
-        <Link
-          to="/login"
-          className="border border-white px-4 py-2 rounded hover:bg-white hover:text-blue-800 w-full text-center"
-        >
-          Sign in
-        </Link>
-        <Link
-          to="/register"
-          className="bg-red-700 px-4 py-2 rounded text-white hover:bg-red-800 w-full text-center"
-        >
-          Register
-        </Link>
+        {userData &&
+          (user ? (
+            <Link
+              to="/dashboard"
+              className="text-white hover:text-gray-300 text-sm font-semibold"
+            >
+              Dashboard
+            </Link>
+          ) : (
+            <div>
+              <Link
+                to="/login"
+                className="border border-white px-4 py-2 rounded hover:bg-white hover:text-blue-800 w-full text-center"
+              >
+                Sign in
+              </Link>
+              <Link
+                to="/register"
+                className="bg-red-700 px-4 py-2 rounded text-white hover:bg-red-800 w-full text-center"
+              >
+                Register
+              </Link>
+            </div>
+          ))}
       </div>
     </nav>
   );
