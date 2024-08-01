2a866b9d896d60479b651ed048ddd3cff1526f36
Ishrafil <ishrafil.webdev@gmail.com>
2024-08-01 19:49:11 +0600

    add create stock adjustment link on dashboard home page

--- src/Pages/Dashboard/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard/Dashboard.jsx
@@ -4,7 +4,7 @@ import ReportCard from "../../../Components/DashboardComponent/ReportCard/Report
 import StoreCard from "../../../Components/DashboardComponent/StoreCard/StoreCard";
 import ShopInvoiceTable from "../../../Components/DashboardComponent/Table/ShopInvoiceTable/ShopInvoiceTable";
 import { GoHome } from "react-icons/go";
-import { useGetAllManufactureQuery } from "../../../features/api/admin/adminManufactureApi";
+// import { useGetAllManufactureQuery } from "../../../features/api/admin/adminManufactureApi";
 
 const reports = [
   {
@@ -44,7 +44,7 @@ const Dashboard = () => {
   return (
     <div>
       <div className="flex items-center gap-x-[10px]">
-        <GoHome className="text-lg"/>
+        <GoHome className="text-lg" />
         <p>Dashboard Home Page</p>
       </div>
 
@@ -53,7 +53,11 @@ const Dashboard = () => {
         <ShopCard title="Purchase Order" color="#955176" />
         <ShopCard title="Total Products" color="#E79C25" />
         <ShopCard title="Stock Items" color="#AE463B" />
-        <ShopCard title="Stock Adjustment" color="#009F8E" />
+        <ShopCard
+          title="Stock Adjustment"
+          color="#009F8E"
+          path="stock-adjustment/create-adjustment"
+        />
       </div>
       <div className="mt-4 grid grid-cols-3 gap-x-8">
         <div className="col-span-1 grid grid-cols-1 gap-y-3">
