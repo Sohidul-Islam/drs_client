35923e1f472bc9bb7287880e0fb6924cffe625c6
Ishrafil <ishrafil.webdev@gmail.com>
2024-09-16 00:08:35 +0600

    add handler for subscription

--- src/Components/PlanCard/PlanCard.jsx
+++ src/Components/PlanCard/PlanCard.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import { FaCircleCheck } from "react-icons/fa6";
+import { useSelector } from "react-redux";
 
 const PlanCard = ({ subscription, index, month }) => {
+  const { user } = useSelector((state) => state.auth);
   const isOdd = index % 2 !== 0;
   const backgroundColor = isOdd ? "bg-[#F0F8FF]" : "";
+  
+  const handleSubscription = (subscriptionId) => {
+    const userSubscriptionData = {
+      userId: user?.id,
+      subscriptionPlanId: subscriptionId,
+    };
+    console.log("user Subscription Data", userSubscriptionData);
+  };
+
   return (
     <div
       className={`relative w-full md:max-w-[302px] md:h-[516px] bg-white border border-[#E2E2E2] rounded-lg shadow-lg p-5 ${backgroundColor}`}
@@ -26,7 +37,10 @@ const PlanCard = ({ subscription, index, month }) => {
           </li>
         ))}
       </ul>
-      <button className="w-[90%] mt-8 border border-[#858585] rounded-md py-2 text-center hover:text-white hover:bg-[#006E9E] absolute bottom-2 left-1/2 transform -translate-x-1/2">
+      <button
+        onClick={() => handleSubscription(subscription?.id)}
+        className="w-[90%] mt-8 border border-[#858585] rounded-md py-2 text-center hover:text-white hover:bg-[#006E9E] absolute bottom-2 left-1/2 transform -translate-x-1/2"
+      >
         Choose Plan
       </button>
     </div>
